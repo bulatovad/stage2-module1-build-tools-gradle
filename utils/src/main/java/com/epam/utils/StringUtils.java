cba5bf5389e303cb4457420bc72c91ed17cd1dd0
bulatovad <damira.bulatova@gmail.com>
2023-05-10 23:51:16 +0600

    solition

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,7 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        int number = Integer.parseInt(str);
+        return  number>0;
     }
 }
